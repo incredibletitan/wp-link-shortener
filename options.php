d37e642e6cf1a37c8eef10e42b8e0efd15952a5a
incredibletitan <100sbsh@gmail.com>
2014-10-27 11:21:45 +0200

    Added settings functionality where user can specified proxy, google api key

---
+++ options.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+    <h2><?php echo $title; ?></h2>
+</div><!--.wrap-->
+
+<form id="shortener-settings-form" method="POST">
+    <div id="" style="float:left;width:100%;">
+    	 <input type="text" name="google_api_key_settings" placeholder="Google api key"><br/>
+        <input type="button" class="button process" value="Save" />
+    </div>
+</form>
